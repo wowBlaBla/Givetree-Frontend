cf5d40846a31b3aa82b1cffad82058cfca95abd3
Dennis Tang <dev@zeroonelabs.tech>
2022-05-04 13:49:33 +1000

    fix: added genopets content creator

---
+++ apps/platform/src/fixtures/campaign/genopets.ts
@@ -0,0 +1,42 @@
+import AssetImg from "../../../assets/images/genopets-asset.png";
+import CollectionImg from "../../../assets/images/genopets-collection.png";
+import { SupportedPlatform } from "../../typed/enum/supportedPlatform";
+import { Campaign } from "../../typed/campaign";
+
+import { genCarbonClimateChangeSociety } from "../charity/carbon-climate-change-society";
+import { genRoyalty } from "../royalties";
+import { genGenopetsContentCreator } from "../content-creator";
+import { genCampaignEvent } from "../event";
+
+import { faker as gen } from "@faker-js/faker";
+
+export const genGenopetsCampaign = (x?: Partial<Campaign>): Campaign => ({
+  id: gen.datatype.uuid(),
+  title: "Genopets",
+  name: "Genopets",
+  shortDescription: gen.lorem.paragraphs(),
+  longDescription: gen.lorem.paragraphs(),
+  media: {
+    campaignBannerUrl: AssetImg.src,
+    campaignCollectionPreviewUrl: AssetImg.src,
+    campaignDetailsUrl: CollectionImg.src,
+    campaignTilePreviewUrl: CollectionImg.src,
+    mintingBannerUrl: AssetImg.src,
+    mintingCollectionPreviewUrl: AssetImg.src,
+  },
+  floorPrice: 3,
+  currency: SupportedPlatform.ETH,
+  totalSupply: 8888,
+  startMintDate: gen.date.future(5),
+  isVerified: true,
+  websiteUrl: gen.internet.url(),
+  discordUrl: gen.internet.url(),
+  twitterUrl: gen.internet.url(),
+  contractUrl: gen.internet.url(),
+  nomatedChartiy: genCarbonClimateChangeSociety(),
+  royalties: genRoyalty(),
+  creators: [genGenopetsContentCreator()],
+  whitelistMemo: gen.datatype.uuid(),
+  event: genCampaignEvent(),
+  ...x,
+});
--- apps/platform/src/fixtures/content-creator/index.ts
+++ apps/platform/src/fixtures/content-creator/index.ts
@@ -1,5 +1,6 @@
 import { ContentCreator } from "../../typed/content-creator";
 import MulgaTheArtistImg from "../../../assets/images/mulga-the-artist.png";
+import GenopetsPreviewImg from "../../../assets/images/genopets-creator.png";
 
 import { faker as gen } from "@faker-js/faker";
 
@@ -11,6 +12,19 @@ export const genMulgaTheArtistContentCreator = (
   description: gen.lorem.paragraphs(2),
   media: {
     previewUrl: MulgaTheArtistImg.src,
+  },
+  isVerified: true,
+  ...x,
+});
+
+export const genGenopetsContentCreator = (
+  x?: Partial<ContentCreator>
+): ContentCreator => ({
+  id: gen.datatype.uuid(),
+  name: "Genopets Official",
+  description: gen.lorem.paragraphs(2),
+  media: {
+    previewUrl: GenopetsPreviewImg.src,
   },
   isVerified: true,
   ...x,
