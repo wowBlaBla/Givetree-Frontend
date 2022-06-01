0114f87636bbb1f5b88bf1140888d02a6353a7d9
tyson-zeroone-labs <tyson.eketone@zeroonelabs.tech>
2022-06-01 16:39:48 +1000

    fix: removed cloduinary api key

--- apps/partner/src/configs/constants.ts
+++ apps/partner/src/configs/constants.ts
@@ -9,8 +9,7 @@ const getEnv = (key: string, defaultValue?: string): string => {
 export const AUTH0_CLIENT_ID = getEnv("REACT_APP_AUTH0_CLIENT_ID");
 export const AUTH0_DOMAIN = getEnv("REACT_APP_AUTH0_DOMAIN");
 export const AUTH0_REDIRECT_URI = getEnv("REACT_APP_AUTH0_REDIRECT_URI");
-export const CLOUDINARY_API_KEY = getEnv("REACT_APP_CLOUDINARY_PRESET");
-export const CLOUDINARY_PRESET = getEnv("REACT_APP_CLOUDINARY_PRESET");
+export const CLOUDINARY_ENDPOINT = getEnv("REACT_APP_CLOUDINARY_ENDPOINT");
 export const HASURA_GRAPHQL_ADMIN_SECRET = getEnv(
   "REACT_APP_HASURA_GRAPHQL_ADMIN_SECRET"
 );
