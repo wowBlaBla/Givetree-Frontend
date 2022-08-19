b93469caebab533781d08bbf3e033f21f9c93684
wowBlaBla <wow1992122@gmail.com>
2022-08-20 02:10:07 +0900

    fix logo size and menu in header

--- apps/platform/src/components/AppFooter.tsx
+++ apps/platform/src/components/AppFooter.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode } from "react";
 import { DiscordSolidIcon } from "./icons/DiscordIcon";
 import { TwitterSolidIcon } from "./icons/TwitterIcon";
 import { YoutubeIcon } from "./icons/YoutubeIcon";
+import { GiveTreeLogo } from "./GiveTreeLogo";
 
 interface SocialLinkProps {
   link: string;
@@ -22,15 +23,17 @@ const SocialLink: FC<SocialLinkProps> = ({ link, socialIcon }) => (
 export const AppFooter = () => (
   <div className="p-2 text-gray-800 bg-white border-t border-black text-black sm:p-3">
     <div className="flex flex-col items-center justify-between gap-2 max-w-screen-xl mx-auto ">
-      <div className="social-group grid md:grid-cols-2 xs:grid-cols-1 md:justify-between flex-wrap gap-5 items-center w-full py-10 justify-center">
-        <div className="mail-input-group flex flex-col gap-2 w-full">
-          <label className="text-xl">Join mailing list</label>
-          <input
-            type="email"
-            className="border border-black w-full max-w-[362px] h-[54px] p-4 rounded-lg-m "
-          />
+      <div className="social-group grid md:grid-cols-2 grid-cols-1 md:justify-between flex-wrap gap-5 items-center w-full py-10 justify-center">
+        <div className="mail-input-group flex  md:justify-start justify-center">
+          <div className="block">
+            <label className="text-xl">Join mailing list</label>
+            <input
+              type="email"
+              className="border border-black w-full max-w-[362px] h-[54px] p-4 rounded-lg-m mt-2 "
+            />
+          </div>
         </div>
-        <div className="social-links flex justify-end">
+        <div className="social-links flex md:justify-end justify-center">
           <div className="flex flex-col gap-2 items-end">
             <label className="text-xl w-full text-left">Join our community</label>
             <div className="flex flex-wrap justify-center gap-3">
@@ -62,11 +65,9 @@ export const AppFooter = () => (
 
       <div className="text-xl grid md:grid-cols-3 sm:grid-cols-2 flex-wrap gap-8 justify-between w-full mt-10">
         <div className="copyright flex flex-col gap-5">
-          <img
-            src="/android-chrome-512x512.png"
-            alt="brand-logo"
-            className="w-16 ratio-square"
-          />
+          <div className="w-32 aspect-square">
+            <GiveTreeLogo className="w-35 h-14-1/2 text-brand-black" withText />
+          </div>
           <div className="flex flex-col gap-2">
             <span>GiveTree where impact is made.</span>
             <span>2022 GiveTree PTY LTD. All rights reserved</span>
--- apps/platform/src/components/AppHeader.tsx
+++ apps/platform/src/components/AppHeader.tsx
@@ -1,65 +1,83 @@
-import React, { FC, ReactElement, ReactNode, useState } from "react";
+import React, { FC, ReactElement, ReactNode, useState, Fragment } from "react";
 import cx from "classnames";
 import { Link, useRoute } from "wouter";
+import { Menu, Transition } from '@headlessui/react'
 import { GiveTreeLogo } from "./GiveTreeLogo";
 import { PlatformRoute } from "../configs/routes";
 import { MenuIcon } from "./icons/MenuIcon";
 import { ConnectWalletButton } from "./wallet/ConnectWalletButton";
 import { CollectionIcon, GlobeIcon, HomeIcon } from "@heroicons/react/outline";
 import { LaunchIcon } from "./icons/LaunchIcon";
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
-interface AppHeaderNavLink {
+interface Dropdown {
   title: string;
   href: PlatformRoute;
   disabled?: boolean;
   icon: ReactElement;
 }
 
+interface AppHeaderNavLink {
+  title: string;
+  href: PlatformRoute;
+  disabled?: boolean;
+  icon: ReactElement;
+  childrens: Array<Dropdown> | undefined;
+  openSidebar?: boolean;
+}
+
 const appHeaderNavLinks: AppHeaderNavLink[] = [
   {
-    title: "Home",
+    title: "Explore",
     href: PlatformRoute.Home,
-    disabled: false,
+    disabled: true,
     icon: <HomeIcon className="w-5 h-5" />,
+    childrens: [
+      {
+        title: "NFT Fundraisers",
+        href: PlatformRoute.Home,
+        disabled: false,
+        icon: <HomeIcon className="w-5 h-5" />,
+      },
+      {
+        title: "Charities",
+        href: PlatformRoute.Home,
+        disabled: false,
+        icon: <HomeIcon className="w-5 h-5" />,
+      },
+      {
+        title: "Creators",
+        href: PlatformRoute.Home,
+        disabled: false,
+        icon: <HomeIcon className="w-5 h-5" />,
+      }
+    ]
   },
   {
-    title: "Mints",
+    title: "Create",
     href: PlatformRoute.CampaignListing,
     disabled: false,
     icon: <LaunchIcon className="w-5 h-5" />,
-  },
-  {
-    title: "Marketplace",
-    href: PlatformRoute.MarketplaceListing,
-    disabled: true,
-    icon: <CollectionIcon className="w-5 h-5" />,
-  },
-  {
-    title: "Charities",
-    href: PlatformRoute.CharityListing,
-    disabled: false,
-    icon: <GlobeIcon className="w-5 h-5" />,
-  },
-  {
-    title: "Creators",
-    href: PlatformRoute.CreatorsListing,
-    disabled: false,
-    icon: <GlobeIcon className="w-5 h-5" />,
+    childrens: undefined,
   },
 ];
 
 interface AppHeaderNavLinkProps {
   children: ReactNode;
+  list: Array<Dropdown> | undefined;
   disabled?: boolean;
   href: PlatformRoute;
   onClick?: () => void;
+  openSidebar?: boolean;
 }
 
 const AppHeaderNavLink: FC<AppHeaderNavLinkProps> = ({
   children,
   disabled,
+  list,
   href,
   onClick,
+  openSidebar
 }) => {
   const [match, _params] = useRoute(href);
 
@@ -79,50 +97,94 @@ const AppHeaderNavLink: FC<AppHeaderNavLinkProps> = ({
           {children}
         </Link>
       ) : (
-        <div
-          className="text-base font-medium text-gray-400 cursor-default tooltip tooltip-bottom whitespace-nowrap"
-          data-tip="Coming soon"
-        >
-          {children}
-        </div>
+        // <div
+        //   className="text-base font-medium text-gray-400 cursor-default tooltip tooltip-bottom whitespace-nowrap"
+        //   data-tip="Coming soon"
+        // >
+        //   {children}
+        // </div>
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <Menu.Button className="text-base font-medium hover:text-brand-orange transition-hover ">
+              <span>{children}</span>
+              <ChevronDownIcon className="-mr-1 ml-1 h-5 w-5 inline-block" aria-hidden="true" />
+            </Menu.Button>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              className={`origin-top-right ${!openSidebar ? "absolute ring-1 ring-black ring-opacity-5 shadow-lg w-56 " : "relative w-42"} right-0 mt-2 rounded-md bg-white divide-y divide-gray-100 focus:outline-none mt-6`}
+            >
+              {
+                list && list.map((item, idx) => {
+                  return (
+                    <div className="py-1">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={cx(
+                              active ? 'bg-gray-100 text-gray-900 font-bold' : 'text-gray-700',
+                              'block px-4 py-4 text-base'
+                            )}
+                          >
+                            {item.title}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  )
+                })
+              }
+            </Menu.Items>
+          </Transition>
+        </Menu>
       )}
     </div>
   );
 };
 
 export const AppHeader: FC = () => {
-  const [openDropdown, setOpenDropdown] = useState<boolean>(false);
+  const [openSidebar, setOpenSidebar] = useState<boolean>(false);
 
   const handleDropdown = () => {
-    setOpenDropdown(!openDropdown);
+    setOpenSidebar(!openSidebar);
   };
 
   return (
     <div className="fixed z-50 w-full py-2 bg-white border-b shadow-sm">
-      <div className="grid w-full grid-cols-2 lg:grid-cols-4 px-3">
+      <div className="grid w-full grid-cols-2 px-3">
         <div className="flex items-center space-x-1 lg:space-x-0">
           <div className="relative cursor-pointer lg:hidden" onClick={handleDropdown}>
             <MenuIcon className="w-7 h-7" />
           </div>
 
-          <Link className="flex items-center cursor-pointer" to={PlatformRoute.Home}>
-            <GiveTreeLogo className="w-32 h-8 lg:h-12 text-brand-black" withText />
+          <Link className="flex items-center cursor-pointer p-2" href={PlatformRoute.Home}>
+            <a><GiveTreeLogo className="w-35 h-14-1/2 text-brand-black cursor-pointer" withText /></a>
           </Link>
-        </div>
-
-        {/* Desktop Navigation */}
-
-        <div className="hidden lg:flex justify-center items-center w-full md:col-span-2 md:space-x-6 lg:space-x-16">
-          {appHeaderNavLinks.map((link, idx) => (
-            <AppHeaderNavLink key={idx} href={link.href} disabled={link.disabled}>
-              {link.title}
-            </AppHeaderNavLink>
-          ))}
         </div>
 
         {/* Wallet */}
 
-        <div className="flex justify-end items-center w-full">
+        <div className="flex gap-10 justify-end items-center w-full">
+          
+          {/* Desktop Navigation */}
+
+          <div className="hidden lg:flex justify-end items-center w-full gap-8">
+            {appHeaderNavLinks.map((link, idx) => (
+              <AppHeaderNavLink key={idx} list={link.childrens} href={link.href} disabled={link.disabled}>
+                {link.title}
+              </AppHeaderNavLink>
+            ))}
+          </div>
           <ConnectWalletButton />
         </div>
       </div>
@@ -133,8 +195,8 @@ export const AppHeader: FC = () => {
         className={cx(
           "absolute h-screen mt-2 bg-white overflow-hidden duration-300 origin-left",
           {
-            "w-48 shadow-lg border-r-2": openDropdown,
-            "w-0": !openDropdown,
+            "w-48 shadow-lg border-r-2": openSidebar,
+            "w-0": !openSidebar,
           }
         )}
       >
@@ -144,7 +206,9 @@ export const AppHeader: FC = () => {
               key={idx}
               href={link.href}
               disabled={link.disabled}
+              list={link.childrens}
               onClick={handleDropdown}
+              openSidebar={openSidebar}
             >
               <div className="flex items-center space-x-1">
                 {link.icon}
