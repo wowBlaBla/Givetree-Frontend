f8d09eee98d36da9af9e792da84affc628e22f26
wowBlaBla <wow1992122@gmail.com>
2022-09-10 09:49:40 +0900

    update signup/signin with web3

--- apps/platform/src/components/modal/AuthModal.tsx
+++ apps/platform/src/components/modal/AuthModal.tsx
@@ -1,12 +1,24 @@
 import { Dialog, Transition } from "@headlessui/react";
 import cx from "classnames";
-import { FC, Fragment, useRef, useState, useCallback } from "react";
+import { FC, Fragment, useRef, useState } from "react";
 import Web3 from "web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import Web3Modal from "web3modal";
 import { useSelector, useDispatch } from "react-redux";
 import { openModal, updateAddress, updateProvider } from "../../store/actions/auth.action";
 import { IStore } from "../../store/reducers/auth.reducer";
+import { MetaMaskIcon } from "../icons/MetaMaskIcon";
+import { WalletConnectIcon } from "../icons/WalletConnectIcon";
+import { CoinbaseIcon } from "../icons/CoinbaseIcon";
+import { PlusIcon } from "@heroicons/react/outline";
+import { ConnectWalletIcon } from "../icons/ConnectWalletIcon";
+import CoinbaseWalletSDK, {CoinbaseWalletProvider} from "@coinbase/wallet-sdk";
+import { XIcon } from "@heroicons/react/solid";
+import { JsonRpcPayload, JsonRpcResponse } from 'web3-core-helpers';
+import { AbstractProvider } from 'web3-core/types';
+
+export declare class WalletConnectWeb3Provider extends WalletConnectProvider implements AbstractProvider {
+    sendAsync(payload: JsonRpcPayload, callback: (error: Error | null, result?: JsonRpcResponse) => void): void;
+}
 
 const navs = [
     "Ethereum",
@@ -14,64 +26,76 @@ const navs = [
     "Solana"
 ];
 
-const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider, // required
-      options: {
-        infuraId: "460f40a260564ac4a4f4b3fffb032dad", // required
-      },
-    },
-}
-
-let web3Modal:Web3Modal;
-    if (typeof window !== 'undefined') {
-    web3Modal = new Web3Modal({
-        network: 'mainnet', // optional
-        cacheProvider: false,
-        providerOptions, // required,
-    })
-}
-
 export const AuthModal:FC = () => {
 
     const openAuthModal = useSelector<IStore, boolean>((state) => state.auth.openAuthModal);
     const dispatch = useDispatch();
     
     const cancelButtonRef = useRef(null)
-    const [activePanel, setActivePanel] = useState(0);
+    const [activePanel, setActivePanel] = useState<string>('0');
 
-    const connect = useCallback(async() => {
+    const connectWalletConnect = async() => {
         try {
-            const provider = await web3Modal.connect();
-            const web3Provider = new Web3(provider);
-            const address = await web3Provider.eth.getAccounts();
-            dispatch(updateProvider(web3Provider));
-            dispatch(openModal(false));
-            dispatch(updateAddress(address[0]));
+            const provider = new WalletConnectProvider({
+                infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+            });
+            await connectAndUpdate(provider as WalletConnectWeb3Provider);
         } catch(err) {
-            console.log(err);
+
         }
-    }, [dispatch])
+    }
+
+    const connectMetaMask = async() => {
+        if (window.ethereum) {
+            await connectAndUpdate(window.ethereum);
+        }
+    }
+
+    const connectCoinbase = async() => {
+        const coinbaseWallet = new CoinbaseWalletSDK({
+            appName: "Givetree"
+        });
+
+        const ethereum = coinbaseWallet.makeWeb3Provider("https://mainnet.infura.io/v3/9278c04944064d5a8f9ad13e549e550c", 1);
+        await connectAndUpdate(ethereum);
+    }
+    
+    const connectAndUpdate = async(provider:WalletConnectWeb3Provider | CoinbaseWalletProvider) => {
+        const accounts = await provider.enable();
+        dispatch(updateProvider(new Web3(provider)));
+        dispatch(openModal(false));
+        dispatch(updateAddress(accounts[0]));
+    }
 
     return (
         <Transition.Root show={openAuthModal} as={Fragment}>
             <Dialog as="div" className="relative z-100" initialFocus={cancelButtonRef} onClose={() => {
                 dispatch(openModal(false));
-                setActivePanel(0);
+                setActivePanel('0');
             }}>
+                
                 <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
                 >
-                <div className="fixed z-100 inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    <div className="fixed z-100 inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
                 </Transition.Child>
 
                 <div className="fixed z-full inset-0 overflow-y-auto">
+                    <span
+                        className="top-0 right-0 z-full cursor-pointer absolute m-4"
+                        onClick={() => {
+                            dispatch(openModal(false));
+                            setActivePanel('0');
+                        }}
+                    >
+                        <XIcon className="w-8 h-8 text-white"/>
+                    </span>
                     <div className="flex items-center justify-center min-h-full p-4 text-center sm:p-0">
                         <Transition.Child
                             as={Fragment}
@@ -82,37 +106,40 @@ export const AuthModal:FC = () => {
                             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
-                        <Dialog.Panel className="relative rounded-lg text-left transform transition-all sm:max-w-lg sm:w-full h-[34rem]">
-                            <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4 h-50">
+                        <Dialog.Panel className="relative rounded-lg text-left transform transition-all sm:max-w-lg sm:w-full">
+                            <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                 <div className="grid text-center">
                                     <div className={cx("flex-col gap-5 items-center sm:mt-0 sm:ml-4", {
-                                        "flex": activePanel == 0,
-                                        "hidden": activePanel != 0,
+                                        "flex": activePanel == '0',
+                                        "hidden": activePanel != '0',
                                     })}>{/* first */}
                                         <Dialog.Title as="h2" className="text-3xl leading-6 font-medium text-white">
                                             Log in or create account
                                         </Dialog.Title>
-                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400" onClick={() => setActivePanel(1)}>Sign Up</button>
-                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400" onClick={() => connect()}>Sign In</button>
+                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400" onClick={() => setActivePanel('up-1')}>Sign Up</button>
+                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400" onClick={() => setActivePanel('in-1')}>Sign In</button>
                                     </div>
                                     <div className={cx("flex-col gap-5 items-center sm:mt-0 sm:ml-4", {
-                                        "flex": activePanel == 1,
-                                        "hidden": activePanel != 1,
+                                        "flex": activePanel == "up-1" || activePanel == "in-1",
+                                        "hidden": activePanel != "up-1" && activePanel != "in-1",
                                     })}>{/* second */}
                                         <Dialog.Title as="h2" className="text-3xl leading-6 font-medium text-white">
-                                            Create Account
+                                            { activePanel == "up-1" ? "Create account" : "Sign in"}
                                         </Dialog.Title>
-                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400" onClick={() => setActivePanel(2)}>Wallet</button>
-                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400" onClick={() => setActivePanel(3)}>Email</button>
+                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400 flex items-center gap-3 justify-center" onClick={() => setActivePanel(activePanel == "up-1" ? "up-2": "in-2")}>
+                                            <ConnectWalletIcon/>
+                                            <span>Wallet</span>
+                                        </button>
+                                        <button className="w-72 h-20 rounded-lg text-lg font-bold text-white bg-cyan-400" onClick={() => setActivePanel(activePanel == "up-1" ? "up-3": "in-3")}>Email</button>
                                     </div>
                                     <div className={cx("flex-col gap-5 items-center sm:mt-0 sm:ml-4", {
-                                        "flex": activePanel == 2,
-                                        "hidden": activePanel != 2,
+                                        "flex": activePanel == "up-2" || activePanel == "in-2",
+                                        "hidden": activePanel != "up-2" && activePanel != "in-2",
                                     })}>
                                         <Dialog.Title as="h2" className="text-3xl leading-6 font-medium text-white">
-                                            Create account
+                                            {activePanel == "up-2" ? "Create account" : "Sign in"}
                                         </Dialog.Title>
-                                        <div className="flex flex-col gap-3 py-10 border rounded-lg bg-gray-600">
+                                        <div className="flex flex-col gap-3 rounded-lg bg-gray-600 p-6">
                                             <h3 className="text-white text-2xl font-bold">Wallet</h3>
                                             <div className="border-b border-white overflow-x-auto scroll-pb-5">
                                                 <div className="max-w-screen-2xl mx-auto w-full">
@@ -149,28 +176,46 @@ export const AuthModal:FC = () => {
                                                     </ul>
                                                 </div>
                                             </div>
-                                            <ul className="flex flex-col items-start gap-4 px-10 py-8">
-                                                <li className="h-8 cursor-pointer text-white font-bold">Metamask</li>
-                                                <li className="h-8 cursor-pointer text-white font-bold">Wallet 2</li>
-                                                <li className="h-8 cursor-pointer text-white font-bold">Wallet 3</li>
-                                                <li className="h-8 cursor-pointer text-white font-bold">Wallet 4</li>
-                                                <li className="h-8 cursor-pointer text-white font-bold">Wallet 5</li>
-                                                <li className="h-8 cursor-pointer text-white font-bold">Wallet 6</li>
-                                            </ul>
+                                            <div
+                                                className="cursor-pointer text-white font-bold shadow-md py-4 px-5 bg-slate-500 rounded-[2px] flex items-center gap-3 hover:bg-slate-400"
+                                                onClick={connectMetaMask}
+                                            >
+                                                <MetaMaskIcon/>
+                                                <span>MetaMask</span>
+                                            </div>
+                                            <div
+                                                className="cursor-pointer text-white font-bold shadow-md py-4 px-5 bg-slate-500 rounded-[2px] flex items-center gap-3 hover:bg-slate-400"
+                                                onClick={connectWalletConnect}
+                                            >
+                                                <WalletConnectIcon/>
+                                                <span>Wallet Connect</span>
+                                            </div>
+                                            <div
+                                                className="cursor-pointer text-white font-bold shadow-md py-4 px-5 bg-slate-500 rounded-[2px] flex items-center gap-3 hover:bg-slate-400"
+                                                onClick={connectCoinbase}
+                                            >
+                                                <CoinbaseIcon className="rounded-full"/>
+                                                <span>Coinbase Wallet</span>
+                                            </div>
+                                            <div className="cursor-pointer text-white font-bold shadow-md py-4 px-5 bg-slate-500 rounded-[2px] flex justify-center gap-2 items-center">
+                                                <span>Show more options</span>
+                                                <PlusIcon className="w-4 h-4"/>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className={cx("flex-col gap-5 items-center sm:mt-0 sm:ml-4", {
-                                        "flex": activePanel == 3,
-                                        "hidden": activePanel != 3,
+                                        "flex": activePanel == "up-3" || activePanel == "in-3",
+                                        "hidden": activePanel != "up-3" && activePanel != "in-3",
                                     })}>
                                         <Dialog.Title as="h2" className="text-3xl leading-6 font-medium text-white">
-                                            Create account
+                                            {activePanel == "up-3" ? "Create account": "Sign in"}
                                         </Dialog.Title>
                                         <div className="flex flex-col gap-3 py-10 border rounded-lg bg-gray-600 px-12 w-full">
                                             <div className="text-white flex flex-col gap-2">
                                                 <h3 className="text-2xl font-bold">Email</h3>
-                                                <h4>Create an account</h4>
-                                                <span>Already have an account? <span>Login</span></span>
+                                                <h4>{activePanel == "up-3" ? "Create an account" : "Sign in"}</h4>
+                                                {activePanel == "up-3" ? <span>Already have an account? <span>Login</span></span> : 
+                                                <span>Don&apos;t have an account? <span>Sign up</span></span> }
                                             </div>
                                             <div className="form-layout flex flex-col gap-2">
                                                 <div className="input-form-group flex flex-col items-start">
