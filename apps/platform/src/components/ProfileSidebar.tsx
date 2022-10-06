124fd562d42cab859bc60d580d3ccc609ea34dd3
dezzzus <alex_dorn@mail.com>
2022-10-06 22:05:24 +0800

    update profile

--- apps/platform/src/components/ProfileSidebar.tsx
+++ apps/platform/src/components/ProfileSidebar.tsx
@@ -11,19 +11,39 @@ import { PrimaryButton } from "./PrimaryCta";
 const navs = [
   {
     category: "home",
-    title: "My Profile",
+    title: "My profile",
+  },
+  {
+    category: "wallet",
+    title: "My wallets",
   },
   {
     category: "nft",
     title: "My NFTs",
   },
   {
-    category: "mint",
-    title: "Create NFT",
+    category: "collection",
+    title: "My NFT collections",
   },
   {
     category: "fundraisers",
     title: "My Fundraisers",
+  },
+  {
+    category: "mint",
+    title: "Create NFT",
+  },
+  {
+    category: "newcollection",
+    title: "Create NFT collection",
+  },
+  {
+    category: "newfundraise",
+    title: "Create fundraiser",
+  },
+  {
+    category: "txhistory",
+    title: "Transaction history",
   },
   {
     category: "settings",
@@ -49,41 +69,44 @@ export const ProfileSideBar: FC = () => {
   }, [walletAddress]);
 
   return (
-    <div className="hidden sm:flex flex-col w-75 py-8 bg-white border-r border-base-content border-opacity-25 dark:bg-mid-dark min-h-screen">
+    <div className="hidden sm:flex flex-col min-w-[240px] py-8 bg-white border-r border-base-content border-opacity-25 dark:bg-mid-dark min-h-screen">
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <div className="flex flex-col items-start mt-6 px-8">
+      <div className="flex flex-col items-center mt-6 px-8">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           className="object-cover w-25 h-25 mx-2 rounded-full"
           src={authedUser?.user.profileImage || avatar.src}
           alt="avatar"
         />
-        <h4 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200 text-base hover:underline">
+        <h4 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200 text-xs hover:underline">
           Mulga The Artist
         </h4>
       </div>
-      <div>
+      {/* <div>
         <PrimaryButton
           className="ml-10 py-2 px-4 mt-4"
           onClick={() => (walletAddress ? disconnect() : setOpenModal(true))}
         >
           {walletAddress ? "Disconnect Wallet" : "Connect Wallet"}
         </PrimaryButton>
-      </div>
-      <div className="flex flex-col justify-between flex-1 mt-6">
+      </div> */}
+      <div className="flex flex-col justify-between flex-1 mt-6 px-4">
         <nav>
           {navs.map((item, idx) => (
-            <Link href={"/profile/" + params?.role + "/" + item.category} key={idx}>
-              <a
-                className={`flex items-center p-4 transition-colors duration-300 transform dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700 ${
+            <Link
+              className={`flex items-center p-2 transition-colors duration-300 transform dark:text-white`}
+              href={"/profile/" + params?.role + "/" + item.category}
+              key={idx}
+            >
+              <div
+                className={`cursor-pointer px-2 py-3 ${
                   item.category == params?.category
-                    ? "bg-menu-orange text-white"
+                    ? "rounded-2xl-1 bg-light-dark text-white"
                     : " text-gray-600 "
                 }`}
-                href=""
               >
-                <span className="mx-4 font-medium">{item.title}</span>
-              </a>
+                <span className={`text-white mx-4 font-medium `}>{item.title}</span>
+              </div>
             </Link>
           ))}
         </nav>
