a18dac303c0bf97b73cb7389576738361ed8fae4
dezzzus <alex_dorn@mail.com>
2022-10-11 02:09:59 +0800

    update Setting page

--- apps/platform/src/containers/profile/creator/Settings.tsx
+++ apps/platform/src/containers/profile/creator/Settings.tsx
@@ -1,34 +1,94 @@
-import { LockClosedIcon } from "@heroicons/react/outline";
-import { FC } from "react";
-import { PrimaryButton } from "../../../components/PrimaryCta";
+import axios from "axios";
+import { FC, useEffect, useRef, useState } from "react";
+import { toast } from "react-toastify";
+import { SwatchesPicker, ColorResult } from "react-color";
+import { useDispatch, useSelector } from "react-redux";
+import { AUTH_USER, IStore } from "../../../store/reducers/auth.reducer";
+import { updateAuthed } from "../../../store/actions/auth.action";
+import { AddIcon } from "../../../components/icons/AddIcon";
 
-export const Settings:FC = () => {
-    return (
-        <div className="p-8">
-            <h2 className="text-2xl font-bold py-7 dark:text-white">Settings</h2>
-            <div className="flex flex-col gap-8 w-[468px]">
-                <div className="input-grop">
-                    <label>Email</label>
-                    <input
-                        type="email"
-                        className="input input-bordered mt-1 block w-full rounded-md shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-12 px-4"
-                    />
-                </div>
-                <div className="input-grop">
-                    <label>Password</label>
-                    <div className="relative text-gray-600 focus-within:text-gray-400">
-                        <input
-                            type="password"
-                            className="input input-bordered mt-1 block w-full rounded-md shadow-sm outline-none focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-12 px-4 pr-10"
-                            />
-                        <span className="absolute inset-y-2 right-0 flex items-center pr-2">
-                            <LockClosedIcon className="w-8 h-8"/>
-                        </span>
-                    </div>
-                </div>
+export const Settings: FC = () => {
+  const authedUser = useSelector<IStore, AUTH_USER | undefined>(
+    (state) => state.auth.authedUser
+  );
+  const dispatch = useDispatch();
 
-            </div>
-            <PrimaryButton className="mt-7 w-20 h-10">Save</PrimaryButton>
+  const [viewType, setViewType] = useState<"edit" | "preview">("edit");
+
+  const [userName, setUserName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+
+  useEffect(() => {
+    if (authedUser && authedUser.user) {
+      setUserName(authedUser.user.userName || "");
+      setEmail(authedUser.user.email || "");
+    }
+  }, [authedUser]);
+
+  const updateProfile = async () => {
+    try {
+      if (authedUser) {
+        const data = {
+          userName,
+          email,
+        };
+
+        const res = await axios.put(
+          `${process.env.NEXT_PUBLIC_API}/api/users/profile`,
+          data,
+          {
+            headers: {
+              Authorization: `Bearer ${authedUser.accessToken}`,
+            },
+          }
+        );
+
+        dispatch(
+          updateAuthed({
+            ...authedUser,
+            user: res.data,
+          })
+        );
+        toast.success("Updated profile successfully!");
+      }
+    } catch (err) {
+      toast.success("Faild updating profile");
+    }
+  };
+
+  return (
+    <div className="profile">
+      <div className="profile-save-section px-8">
+        <div className="flex justify-between max-w-[632px] items-center justify-end h-[51px]">
+          <div className="flex-1" />
+          <button
+            className="btn bg-[#0075FF] text-white h-[30px] min-h-0"
+            onClick={updateProfile}
+          >
+            Save
+          </button>
         </div>
-    )
-}
+      </div>
+      <div className="p-8 max-w-[700px]">
+        <div className="profile-section">
+          <label className="mb-1 text-md text-white">Email</label>
+          <input
+            readOnly
+            type="email"
+            className="input input-bordered profile-item mt-1 block w-full outline-none"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label className="mb-1 text-md text-white">Username</label>
+          <input
+            type="text"
+            readOnly
+            className="input input-bordered profile-item mt-1 block w-full outline-none"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
