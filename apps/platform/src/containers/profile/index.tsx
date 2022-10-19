bd4d0627d3aeabfd43b85bf93998b131e9d25f36
dezzzus <alex_dorn@mail.com>
2022-10-19 18:31:35 +0800

    update profile

--- apps/platform/src/containers/profile/index.tsx
+++ apps/platform/src/containers/profile/index.tsx
@@ -4,16 +4,20 @@ import { ProfileSideBar } from "./ProfileSidebar";
 import { Fundraisers } from "./Fundraisers";
 import { Mint } from "./Mint";
 import { ProfileNFTs } from "./Nfts";
-import { Profile } from "./Profile";
+import { Appearance } from "./Appearance";
 import { Settings } from "./Settings";
+import { HomeSettings } from "./HomeSettings";
 
 const ProfilePortal: FC = () => {
   return (
     <div className="profile-container flex">
       <ProfileSideBar />
       <div className="content-wrapper w-full">
-        <Route path="/profile/home">
-          <Profile />
+        <Route path="/profile/home-appearance">
+          <Appearance />
+        </Route>
+        <Route path="/profile/home-settings">
+          <HomeSettings />
         </Route>
         <Route path="/profile/nft">
           <ProfileNFTs />
