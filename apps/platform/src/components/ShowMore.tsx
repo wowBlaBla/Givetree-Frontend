3986a8ccb859fe59f59b1cbb7c8db07cfb0d9b26
dezzzus <alex_dorn@mail.com>
2022-10-31 03:40:31 +0800

    update collection page

--- apps/platform/src/components/ShowMore.tsx
+++ apps/platform/src/components/ShowMore.tsx
@@ -1,32 +1,24 @@
 import { FC, useState } from "react";
 
 interface Props {
-    text: string;
+  text: string;
 }
 
-export const ShowMore:FC<Props> = ({ text }) => {
-    const [expand, setExpand] = useState(false);
-    return (
-        <div className="flex flex-col gap-2 dark:text-white">
-            <span>
-                {
-                    text.length > 100 ? (
-                        <>
-                            {
-                                !expand ? text.slice(0, 100) + '...'  : text
-                            }
-                        </>
-                    ) : text
-                }
-            </span>
-            {
-                text.length > 100 && (
-                    <span
-                        className="font-bold cursor-pointer hover:text-sky-300"
-                        onClick={() => setExpand(!expand)}
-                    >Read {expand ? "less": "more"}</span>
-                )
-            }
-        </div>
-    )
-}
+export const ShowMore: FC<Props> = ({ text }) => {
+  const [expand, setExpand] = useState(false);
+  return (
+    <div className="flex flex-col gap-2">
+      <span>
+        {text.length > 100 ? <>{!expand ? text.slice(0, 100) + "..." : text}</> : text}
+      </span>
+      {text.length > 100 && (
+        <span
+          className="font-bold cursor-pointer hover:text-sky-300"
+          onClick={() => setExpand(!expand)}
+        >
+          Show {expand ? "less" : "more"}
+        </span>
+      )}
+    </div>
+  );
+};
