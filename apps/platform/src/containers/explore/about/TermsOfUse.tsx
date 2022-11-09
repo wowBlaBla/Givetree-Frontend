b6afa0eb7d46212e10ec91e871beedd27ce7aa20
dezzzus <alex_dorn@mail.com>
2022-11-09 09:50:15 +0800

    update signup with wallet and about page

---
+++ apps/platform/src/containers/explore/about/TermsOfUse.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+
+export const TermsOfUsePage: FC = () => {
+  return (
+    <div className="p-8">
+      <h1 className="font-bold text-lg text-black">Terms of use</h1>
+      <div className="rounded-2xl-1 border border-[#696969] bg-white px-8 py-5 max-w-[450px] mt-4">
+        <span className="text-black">
+          Lorum impsum lorum ipsum lorum ipsum Lorum impsum lorum ipsum lorum ipsum Lorum
+          impsum lorum ipsum lorum ipsum Lorum impsum lorum ipsum lorum ipsum Lorum impsum
+          lorum ipsum lorum ipsum Lorum impsum lorum ipsum lorum ipsum Lorum impsum lorum
+          ipsum lorum ipsum Lorum impsum lorum ipsum lorum ipsum Lorum impsum lorum ipsum
+          lorum ipsum Lorum impsum lorum ipsum lorum ipsum Lorum impsum lorum ipsum lorum
+          ipsum Lorum impsum lorum ipsum lorum ipsumipsum lorum ipsum Lorum impsum lorum
+          ipsum lorum ipsum Lorum impsum lorum ipsum lorum ipsum Lorum impsum lorum ipsum
+          lorum ipsum Lorum impsum lorum ipsum lorum ipsum ipsum lorum ipsum Lorum impsum
+          lorum ipsum lorum ipsum Lorum impsum lorum ipsum lorum ipsum Lorum impsum lorum
+          ipsum lorum ipsum Lorum impsum lorum ipsum lorum ipsum
+        </span>
+      </div>
+    </div>
+  );
+};
