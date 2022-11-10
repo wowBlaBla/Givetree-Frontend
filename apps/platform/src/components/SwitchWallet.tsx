5c3491ac2207bc1516f53ef692b6bd433c925d4b
dezzzus <alex_dorn@mail.com>
2022-11-11 06:25:34 +0800

    fix deploy issue

--- apps/platform/src/components/SwitchWallet.tsx
+++ apps/platform/src/components/SwitchWallet.tsx
@@ -2,37 +2,36 @@ import { FC } from "react";
 import { useWallet } from "../context/WalletContext";
 
 interface Props {
-    title: string;
+  title: string;
 }
 
-export const SwitchWallet:FC<Props> = ({ title }) => {
-    const { address, connectWallet, reset } = useWallet();
+export const SwitchWallet: FC<Props> = ({ title }) => {
+  const { address, connectWallet, reset } = useWallet();
 
-    return (
-        <>
-            <span>{title}</span>
-            <div className="flex my-6">
-                <input
-                    readOnly
-                    value={address || ""}
-                    type="text"
-                    className="input input-bordered block w-full outline-none bg-white border-[#5B626C] max-w-[400px]"
-                />
-                
-                {
-                    address ? (
-                        <button
-                            className="btn btn-error btn-connect ml-2"
-                            onClick={reset}
-                        >Disconnect</button>
-                    ) : (
-                        <button
-                            className="btn btn-primary btn-connect ml-2"
-                            onClick={() => connectWallet("metamask", "switch")}
-                        >Connect</button>
-                    )
-                }
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <span>{title}</span>
+      <div className="flex my-6">
+        <input
+          readOnly
+          value={address || ""}
+          type="text"
+          className="input input-bordered block w-full outline-none bg-white border-[#5B626C] max-w-[400px]"
+        />
+
+        {address ? (
+          <button className="btn btn-error btn-connect ml-2" onClick={reset}>
+            Disconnect
+          </button>
+        ) : (
+          <button
+            className="btn btn-primary btn-connect ml-2"
+            onClick={() => connectWallet("metamask")}
+          >
+            Connect
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
